c4a01b37ad9c57aead874c87516efc462b2b9c90
Taison <pedrini.taison@gmail.com>
2025-11-17 08:19:27 -0300

    Add files via upload

---
+++ src/main/java/com/complexo/UNIFEBE_Complexo_Esportivo/dao/Usuario.java
@@ -0,0 +1,37 @@
+package com.complexo.UNIFEBE_Complexo_Esportivo.dao;
+
+public class Usuario {
+    private String nome;
+    private int matricula;
+    private int id_USUARIO;
+    private String senha;
+    private char tipo; // 'A' para Admin, 'C' para Comum
+
+    public Usuario() {}
+
+    public Usuario(String nome, int matricula, String senha, char tipo) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public int getId_USUARIO() {
+        return id_USUARIO;
+    }
+    public void setId_USUARIO(int id_USUARIO) {
+        this.id_USUARIO = id_USUARIO;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public int getMatricula() { return matricula; }
+    public void setMatricula(int matricula) { this.matricula = matricula; }
+
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public char getTipo() { return tipo; }
+    public void setTipo(char tipo) { this.tipo = tipo; }
+}
---
+++ src/test/java/com/complexo/UNIFEBE_Complexo_Esportivo/UnifebeComplexoEsportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complexo.UNIFEBE_Complexo_Esportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifebeComplexoEsportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
